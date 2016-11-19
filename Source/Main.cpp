47af83204fcd88008c26f44bafcc5be37288fc8c
Your Name <mhopson@hotmail.co.uk>
2016-11-19 18:38:25 +0000

    Remove headers from main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,19 +3,10 @@
 #include <string>
 
 #include "Application.h"
-
-#include "Util/Random.h"
-#include "Util/Debug_Display.h"
 #include "Util/Display.h"
-#include "Util/Noise_Generator.h"
-
-#include "World/Gen/World_Generator.h"
-#include "World/Block/D_Blocks.h"
-
 
 int main()
 {
-    //Create the OpenGL Context
     Display::create("MattCraft");
 
     Application app;
