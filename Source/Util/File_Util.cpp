1213c63201dace7a9d01b5bb4af6073890d9fd01
Hopson97 <mhopson@hotmail.co.uk>
2017-04-27 19:01:15 +0100

    Add more copyrighted songs lol

--- Source/Util/File_Util.cpp
+++ Source/Util/File_Util.cpp
@@ -5,37 +5,9 @@
 
 #ifdef __WIN32
     #include <windows.h>
-#endif // __WIN32
-
-namespace
-{
-    #ifdef __WIN32
-    std::vector<std::string> getFileNamesFromFolderLocal(const std::string& folderName)
-    {
-        #ifdef __WIN32
-            std::vector<std::string> fileNames;
-            std::string searchFor (folderName + "/*.*");
-            WIN32_FIND_DATA findData;
-            auto hFind = ::FindFirstFile(searchFor.c_str(), &findData);
-
-            if (hFind != INVALID_HANDLE_VALUE)
-            {
-                do
-                {
-                    if(!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-                    {
-                        fileNames.push_back(findData.cFileName);
-                    }
-
-                } while (::FindNextFile(hFind, &findData));
-            }
-
-            return fileNames;
-        #endif // __WIN32
-    }
-    #endif // __WIN32
-}
-
+#elif __LINUX__
+    #include <sys/types.h>
+#endif // __LINUX__
 
 std::string getFileContents(const std::string& filePath)
 {
@@ -46,34 +18,53 @@ std::string getFileContents(const std::string& filePath)
     return stream.str();        //Return the string from the string stream
 }
 
+
+
+
 #ifdef __WIN32
-///@TODO More OS implementations of this function
-std::vector<std::string> getFileNamesFromFolder(const std::string& folderName)
-{
-    std::vector<std::string> fileNames;
-    std::string searchFor (folderName + "/*.*");
-    WIN32_FIND_DATA findData;
-    auto hFind = ::FindFirstFile(searchFor.c_str(), &findData);
-
-    if (hFind != INVALID_HANDLE_VALUE)
+    std::vector<std::string> getFileNamesFromFolder(const std::string& folderName)
     {
-        do
+        std::vector<std::string> fileNames;
+        std::string searchFor (folderName + "/*.*");
+        WIN32_FIND_DATA findData;
+        auto hFind = ::FindFirstFile(searchFor.c_str(), &findData);
+
+        if (hFind != INVALID_HANDLE_VALUE)
         {
-            if(!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+            do
             {
-                fileNames.push_back(findData.cFileName);
-            }
+                if(!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+                {
+                    fileNames.push_back(findData.cFileName);
+                }
 
-        } while (::FindNextFile(hFind, &findData));
+            } while (::FindNextFile(hFind, &findData));
+        }
+
+        return fileNames;
     }
+#elif __linux__
+    std::vector<std::string> getFileNamesFromFolder(const std::string& folderName)
+    {
+        std::vector<std::string> fileNames;
 
-    return fileNames;
-}
+        DIR *dp;
+        struct dirent *dirp;
+        if((dp  = opendir(dir.c_str())) == NULL) {
+            std::cout << "Error(" << errno << ") opening " << dir << std::endl;
+            return errno;
+        }
 
-#else
-
+        while ((dirp = readdir(dp)) != NULL) {
+            fileNames.push_back((dirp->d_name));
+        }
+        closedir(dp);
+        return fileNames;
+    }
+ifdef __APPLE__
 std::vector<std::string> getFileNamesFromFolder(const std::string& folderName)
 {
+    ///@TODO Apple implementation of this
     return {};
 }
 
