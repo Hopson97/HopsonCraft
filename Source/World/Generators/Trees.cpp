921a5cd320d5c7389b4842b7260fe78d59edeee2
Hopson97 <mhopson@hotmail.co.uk>
2017-05-26 22:32:42 +0100

    move acacida tree gen into cpp file

--- Source/World/Generators/Trees.cpp
+++ Source/World/Generators/Trees.cpp
@@ -141,6 +141,71 @@ namespace Structure
         tree.build(access);
     }
 
+    void makeAcaciaTree(IBlock_Accessible& access,
+                        const Block::Position& pos,
+                        Random::Generator<std::mt19937>& random)
+    {
+        Tree_Helper tree(Block::ID::Oak_Leaf,
+                         Block::ID::Oak_Wood);
+
+        auto height = random.intInRange(5, 8);
+        auto width1 = random.intInRange(4, 5);
+        auto dir1 = random.intInRange(0, 1);
+
+        for (int32_t y = 1; y < height; y++)
+        {
+            tree.addWood(pos.x, pos.y + y, pos.z);
+        }
+
+        float y = pos.y + height;
+        switch(dir1)
+        {
+            case 0:
+                for (int32_t i = 0; i <= width1; i++)
+                {
+                    tree.addWood(pos.x, (int)std::floor(y), pos.z + i);
+                    y += 0.5;
+                }
+                break;
+
+            case 1:
+                for (int32_t i = 0; i <= width1; i++)
+                {
+                    tree.addWood(pos.x + i, (int)std::floor(y), pos.z);
+                    y += 0.5;
+                }
+                break;
+        }
+
+        auto dir2 = random.intInRange(0, 1);
+
+        for (int32_t y = 1; y < height; y++)
+        {
+            tree.addWood(pos.x, pos.y + y, pos.z);
+        }
+
+        y = pos.y + height;
+        switch(dir2)
+        {
+            case 0:
+                for (int32_t i = 0; i <= width1; i++)
+                {
+                    tree.addWood(pos.x, (int)std::floor(y), pos.z + i);
+                    y += 0.5;
+                }
+                break;
+
+            case 1:
+                for (int32_t i = 0; i <= width1; i++)
+                {
+                    tree.addWood(pos.x + i, (int)std::floor(y), pos.z);
+                    y += 0.5;
+                }
+                break;
+        }
+    }
+
+
 
 }
 
--- Source/World/Generators/Trees.h
+++ Source/World/Generators/Trees.h
@@ -19,70 +19,9 @@ void makePalmTree(IBlock_Accessible& access,
                   const Block::Position& pos,
                   Random::Generator<std::mt19937>& random);
 
-template<typename Access, typename Rand>
-void makeAcaciaTree(Access& access,
-                 const Block::Position& pos,
-                 Random::Generator<Rand> random,
-                 bool isMadeDuringWorldGen = true)
-{/*
-    auto height = random.intInRange(5, 8);
-    auto width1 = random.intInRange(4, 5);
-    auto dir1 = random.intInRange(0, 1);
-
-    for (int32_t y = 1; y < height; y++)
-    {
-        access.qSetBlock({pos.x, pos.y + y, pos.z}, Block::ID::Oak_Wood, isMadeDuringWorldGen);
-    }
-
-    float y = pos.y + height;
-    switch(dir1)
-    {
-        case 0:
-            for (int32_t i = 0; i <= width1; i++)
-            {
-                access.setBlock({pos.x, (int)std::floor(y), pos.z + i}, Block::ID::Oak_Wood, isMadeDuringWorldGen);
-                y += 0.5;
-            }
-            break;
-
-        case 1:
-            for (int32_t i = 0; i <= width1; i++)
-            {
-                access.setBlock({pos.x + i, (int)std::floor(y), pos.z}, Block::ID::Oak_Wood, isMadeDuringWorldGen);
-                y += 0.5;
-            }
-            break;
-    }
-
-    auto dir2 = random.intInRange(0, 1);
-
-    for (int32_t y = 1; y < height; y++)
-    {
-        access.qSetBlock({pos.x, pos.y + y, pos.z}, Block::ID::Oak_Wood, isMadeDuringWorldGen);
-    }
-
-    y = pos.y + height;
-    switch(dir2)
-    {
-        case 0:
-            for (int32_t i = 0; i <= width1; i++)
-            {
-                access.setBlock({pos.x, (int)std::floor(y), pos.z - i}, Block::ID::Oak_Wood, isMadeDuringWorldGen);
-                y += 0.5;
-            }
-            break;
-
-        case 1:
-            for (int32_t i = 0; i <= width1; i++)
-            {
-                access.setBlock({pos.x - i, (int)std::floor(y), pos.z}, Block::ID::Oak_Wood, isMadeDuringWorldGen);
-                y += 0.5;
-            }
-            break;
-    }
-*/
-    ///@TODO Leaves for the Acacia tree
-}
+void makeAcaciaTree(IBlock_Accessible& access,
+                    const Block::Position& pos,
+                    Random::Generator<std::mt19937>& random);
 
 }
 
